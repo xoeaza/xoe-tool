80f14ed7804bccef7659d5c194e695e3beec291f
xiaobo.huang <xoeaza@gmail.com>
2018-09-19 15:36:43 +0800

    feat: typeof null 是 object 原因

--- 基本类型判断.js
+++ 基本类型判断.js
@@ -1,3 +1,5 @@
+// 在 JS 最初版本中，使用的是 32 位系统，为了性能考虑使用低位存储了变量的类型信息，000 开头代表是对象.然而 null 表示为全零，所以被错误的判断为object。虽然现在的内部类型判断代码已经改变，但是这Bug却一直流传下来。
+typeof null // 'object'
 // 如果想获得一个变量的正确类型，可以通过 Object.prototype.toString.call(xx)。这样就可以获得类似 [object Type] 的字符串
 let a = {}, //[object Object]
   b = undefined, //[object Undefined]
