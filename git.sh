c0a897352c7328807a711bde7d2c08f62a7555dc
xoeaza <xoeaza@gmail.com>
2019-06-03 11:31:44 +0800

    feat: add git.sh

---
+++ git.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="huangxiaobo@vipkid.com.cn"
+CORRECT_NAME="xiaobo.huang"
+CORRECT_EMAIL="xoeaza@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+# 赋予执行权限 chmod +x email.sh
+# 运行 ./email.sh
+# 覆盖远端 git push origin --force --all
+#         git push origin --force --tags
