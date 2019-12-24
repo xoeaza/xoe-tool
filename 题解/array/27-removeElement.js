7984e851643083926b506e976e3113d95a546029
xiaobo.huang <xiaobo.huang@renren-inc.com>
2019-12-24 16:30:49 +0800

    feat: 更新27-removeElement

---
+++ 题解/array/27-removeElement.js
@@ -0,0 +1,17 @@
+// 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+// 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+// 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+// 示例 1:
+// 给定 nums = [3,2,2,3], val = 3,
+// 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+// 你不需要考虑数组中超出新长度后面的元素。
+
+const removeElement = function(nums, val) {
+    let point = 0
+
+    for (let i = 0, j = nums.length; i < j; ++i) {
+        if (nums[i] !== val) nums[point++] = nums[i]
+    }
+
+    return point
+}
