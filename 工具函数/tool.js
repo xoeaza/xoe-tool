643f9249ff93674dcc93d2710657b94ed5b0f3a8
xiaobo.huang <xiaobo.huang@renren-inc.com>
2020-01-08 10:38:32 +0800

    feat: add arr remove

--- 工具函数/tool.js
+++ 工具函数/tool.js
@@ -293,3 +293,14 @@ export default function getOuterSizes(element) {
     return result;
   }
   
+/**
+ * Remove an item from an array
+ */
+export function remove (arr, item) {
+  if (arr.length) {
+    const index = arr.indexOf(item)
+    if (index > -1) {
+      return arr.splice(index, 1)
+    }
+  }
+}
