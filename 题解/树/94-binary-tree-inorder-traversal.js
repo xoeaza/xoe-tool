810d660340682e241a487bb87dd3df9534936ce8
xiaobo.huang <xiaobo.huang@renren-inc.com>
2020-03-13 10:22:03 +0800

    feat: 添加二叉树中序遍历

---
+++ 题解/树/94-binary-tree-inorder-traversal.js
@@ -0,0 +1,35 @@
+/* 给定一个二叉树，返回它的中序 遍历。
+示例:
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+输出: [1,3,2]
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = []
+
+    function iter(node) {
+        if (node == null) return null
+        iter(node.left)
+        res.push(node.val)
+        iter(node.right)
+    }
+    iter(root)
+    return res
+}
