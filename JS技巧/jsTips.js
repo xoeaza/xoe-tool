aff2c37ffb734f61c652d86ad9d5e9ce75529ef1
xiaobo.huang <xiaobohuang@didiglobal.com>
2021-05-06 22:06:21 +0800

    feat: 添加枚举小技巧

--- JS技巧/jsTips.js
+++ JS技巧/jsTips.js
@@ -13,8 +13,32 @@ const disabledConfirm = (() => {
 console.log(disabledConfirm); // true
 
 // map - Promise.all
-// 使用async await 处理异步操作
-let results = await Promise.all(arr.map(async (item) => {
-	// 等待异步操作完成，返回执行结果
-	return await asyncWorker(item);
-}));
+//2. 使用async await 处理异步操作
+let results = await Promise.all(
+  arr.map(async item => {
+    // 等待异步操作完成，返回执行结果
+    return await asyncWorker(item);
+  })
+);
+
+//3. 单一数据源的取值定义
+export const DATA_TYPE = {
+  NAME1: {
+    id: 1,
+    label: '111'
+  },
+  NAME2: {
+    id: 2,
+    label: '222'
+  }
+};
+
+export const DATA_TYPE_LIST = Object.keys(DATA_TYPE).map(val => {
+  return DATA_TYPE[val];
+});
+
+export const DATA_TYPE_MAP = {};
+Object.keys(DATA_TYPE).forEach(val => {
+  const { id, label } = DATA_TYPE[val];
+  DATA_TYPE_MAP[id] = label;
+});
