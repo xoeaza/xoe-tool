d1e415cd62d0664931dbbdc04a71d24ccf428ee5
xiaobo.huang <xiaobohuang@didiglobal.com>
2022-05-04 12:01:32 +0800

    feat: 优化tinyImg脚本

--- js脚本/tinyImg.js
+++ js脚本/tinyImg.js
@@ -1,45 +1,49 @@
 //使用tinify压缩图片
-const path = require('path');
-const fs = require('fs');
-const tinify = require('tinify');
-const chalk = require('chalk');
+import path from 'path'
+import fs from 'fs'
+import chalk from 'chalk'
+import tinify from 'tinify'
 
 // 可于https://tinypng.com/developers申请
-tinify.key = 'eVlmdqRD6CSj5Dy5U8J5yuVhI0rDFvXf';
-const filePath = 'src/assets';
-const files = fs.readdirSync(filePath);
-const reg = /\.(jpg|png)$/;
-console.log(chalk.yellow(`上传TinyPNG中...`));
+tinify.key = 'eVlmdqRD6CSj5Dy5U8J5yuVhI0rDFvXf'
+const filePath = 'src/assets'
+const files = fs.readdirSync(filePath)
+const reg = /\.(jpg|png|jpeg)$/
+console.log(chalk.yellow(`上传TinyPNG中...`))
 
-async function compress() {
+async function compress(filePath, files) {
   for (let file of files) {
-    let filePathAll = path.join(filePath, file);
-    let stats = fs.statSync(path.join(filePath, file));
+    let filePathAll = path.join(filePath, file)
+    let stats = fs.statSync(path.join(filePath, file))
     if (!stats.isDirectory() && reg.test(file)) {
       await new Promise((resolve, reject) => {
         fs.readFile(filePathAll, (err, sourceData) => {
           if (err) {
-            console.log(chalk.red(`${file} 压缩失败`));
-            reject(err);
+            console.log(chalk.red(`${file} 压缩失败`))
+            reject(err)
           } else {
-            let fileSize = fs.statSync(filePathAll).size;
+            let fileSize = fs.statSync(filePathAll).size
             tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
               if (err) {
-                console.log(chalk.red(`${file} 压缩失败`));
-                reject(err);
+                console.log(chalk.red(`${file} 压缩失败`))
+                reject(err)
               }
               //将压缩后的文件保存覆盖
               fs.writeFile(filePathAll, resultData, () => {
-                let compressFileSize = fs.statSync(filePathAll).size;
-                console.log(chalk.green(`${file} ${(fileSize / 1024).toFixed(2)}kb 压缩成功 ${(compressFileSize / 1024).toFixed(2)}kb ${((compressFileSize - fileSize) * 100 / fileSize).toFixed(1)}%`));
-                resolve();
+                let compressFileSize = fs.statSync(filePathAll).size
+                console.log(chalk.green(`${filePathAll} ${file} ${(fileSize / 1024).toFixed(2)}kb 压缩成功 ${(compressFileSize / 1024).toFixed(2)}kb ${(((compressFileSize - fileSize) * 100) / fileSize).toFixed(1)}%`))
+                resolve()
               })
             })
           }
         })
       })
+    } else if (stats.isDirectory()) {
+      const fileList = fs.readdirSync(`${filePath}${file}/`)
+      console.log(fileList, fileList.length, '===>fileList')
+      await compress(`${filePath}${file}/`, fileList)
     }
   }
 }
 
-compress();
+compress(filePath, files)
