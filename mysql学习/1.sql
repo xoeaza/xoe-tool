535a7023f3707c41fc89ca073853554aa93e119d
xiaobo.huang <xiaobo.huang@cicc.com.cn>
2023-10-26 17:19:29 +0800

    feat: 添加用户管理

--- mysql学习/1.sql
+++ mysql学习/1.sql
@@ -347,3 +347,30 @@ where (a.peopleId,a.seq) in (select peopleId,seq from vitae group by peopleId,se
 -- 查找表中多余的重复记录（多个字段），不包含rowid最小的记录
 select * from vitae a
 where (a.peopleId,a.seq) in (select peopleId,seq from vitae group by peopleId,seq having count(*) > 1) and rowid not in (select min(rowid) from vitae group by peopleId,seq having count(*)>1)
+
+-- 创建用户 CREATE USER 用户名 IDENTIFIED BY '密码'
+CREATE USER kuangshen IDENTIFIED BY '123456'
+
+-- 修改密码（修改当前用户密码）
+SET PASSWORD = PASSWORD('123456')
+
+-- 修改密码（修改指定用户密码）
+SET PASSWORD FOR kuangshen = PASSWORD('123456')
+
+-- 重命名 RENAME USER 原来名字TO新的名字
+RENAME USER kuangshen TO kuangshen2
+
+-- 用户授权ALL PRIVILEGES全部的权限，库.表
+-- ALL PRIVILEGES除了给别人授权，其他都能够干
+GRANT ALL PRIVILEGES ON *.* TO kuangshen2
+
+-- 查询权限
+SHOW GRANTS FOR kuangshen2 --查看指定用户的权限
+SHoW GRANTS FOR root@localhost
+
+-- ROOT用户权限：GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION
+-- 撤销权限 REVOKE 哪些权限，在哪个库撤销，给谁撤销
+REVOKE ALL PRIVILEGES ON *.* FROM kuangshen2
+
+-- 删除用户
+DROP USER kuangshen
